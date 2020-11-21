06e86f024fc91fca3676109a249b5b6c3636fcca
Andre Parsons <andrparsons@gmail.com>
2020-11-21 17:59:21 -0500

    added dayjs to parse time

--- components/JobCard/JobCard.js
+++ components/JobCard/JobCard.js
@@ -1,4 +1,8 @@
 import styles from "./JobCard.module.css";
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+
+dayjs.extend(relativeTime);
 
 const JobCard = ({
   job: { company_logo, created_at, type, title, company, location },
@@ -8,7 +12,7 @@ const JobCard = ({
       <div className={styles.cardContainer}>
         <img className={styles.companyLogo} src={company_logo} />
         <div className={styles.subtitle}>
-          <p>{created_at}</p>
+          <p>{dayjs(created_at).fromNow()}</p>
           <p>{type}</p>
         </div>
         <h3>{title}</h3>

d44749c5722bf62ad7a845b1b6b1708685201711
Andre Parsons <andrparsons@gmail.com>
2020-11-26 17:30:45 -0500

    test fetch function

--- hooks/use-jobs.js
+++ hooks/use-jobs.js
@@ -4,7 +4,7 @@ const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function useJobs() {
   return useSWR(
-    "https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json",
+    "https://jobs.github.com/positions.json",
     fetcher
   );
 }
